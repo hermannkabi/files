6bf5ba7e9f6dcb4fdadb10f1ae66b86b3c469929
Hermann <hermann.kaebi@gmail.com>
2021-03-07 09:39:26 +0200

    Made app correctly launch QuantoAuth

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -14,9 +14,9 @@ const RESOURCES = {
 "favicon.png": "1b7ab9d46e913bbb568867930e2bd3fb",
 "icons/Icon-192.png": "1b7ab9d46e913bbb568867930e2bd3fb",
 "icons/Icon-512.png": "0cea195b6c0fa4a534935b0ec5f77ec7",
-"index.html": "70926e69fa3456fdf993f0447c51083e",
-"/": "70926e69fa3456fdf993f0447c51083e",
-"main.dart.js": "cd16eb0ff52b323b2db82436db312366",
+"index.html": "de05718cdb90fb80969747a01d3a02ac",
+"/": "de05718cdb90fb80969747a01d3a02ac",
+"main.dart.js": "ff310f9dbc17d1ebd3ca78ca51801086",
 "manifest.json": "cbbda7da8ea7956dce7aa8e0d13dd954",
 "version.json": "fe885575f08091230ea1c432546d6fce"
 };
